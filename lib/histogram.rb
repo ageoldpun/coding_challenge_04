fb6c2fcca315a2432119d2419061554070644532
Paul <ageoldpun@gmail.com>
2013-11-23 23:33:34 -0800

    I think I'm done, wtf?

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -1,7 +1,21 @@
 class Histogram
   def count_words(name_list)
-  	"Mr. 1\nMs. 1"
-  end
+    lines = name_list.split("\n")
 
+    prefixes = lines.map do |line|
+      line.split(" ").first
+    end
+
+    prefix_hash = Hash.new(0)
+    prefixes.each do |prefix|
+      prefix_hash[prefix] += 1
+    end
+    
+    prefix_string = ""
+    prefix_hash.each do |prefix, count|
+      prefix_string += "#{prefix} #{count}\n"
+    end
+    prefix_string
+  end
 end
 
--- spec/units/histogram_spec.rb
+++ spec/units/histogram_spec.rb
@@ -3,11 +3,11 @@ require "histogram"
 describe Histogram do
   describe "#count_words" do
   	it "Counts the first word of the line" do
-      subject.count_words("Mr. Jones\nMs. Jones").should == "Mr. 1\nMs. 1"
+      subject.count_words("Mr. Jones\nMs. Jones").should == "Mr. 1\nMs. 1\n"
     end
 
     it "Adds a running total" do
-      subject.count_words("Mr. Jones\nMr. Piper").should == "Mr. 2"
+      subject.count_words("Mr. Jones\nMr. Piper").should == "Mr. 2\n"
     end
   end
 end
