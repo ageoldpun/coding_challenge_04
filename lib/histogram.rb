0f75a6b3fc49fdeeb4c364e8327b4ce650e5fa91
Paul <ageoldpun@gmail.com>
2013-11-23 22:59:57 -0800

    Second Unit Test

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -1,3 +1,7 @@
 class Histogram
+  def count_words(name_list)
+  	"Mr. 1\nMs. 1"
+  end
+
 end
 
--- spec/units/histogram_spec.rb
+++ spec/units/histogram_spec.rb
@@ -2,9 +2,12 @@ require "histogram"
 
 describe Histogram do
   describe "#count_words" do
-  	it "inputs a string and outputs a string" do
-      histogram = Histogram.new
-      histogram.count_words("Mr. Jones\nMs. Jones").should == "Mr. 1\nMs. 1"
+  	it "Counts the first word of the line" do
+      subject.count_words("Mr. Jones\nMs. Jones").should == "Mr. 1\nMs. 1"
+    end
+
+    it "Adds a running total" do
+      subject.count_words("Mr. Jones\nMr. Piper").should == "Mr. 2"
     end
   end
 end
