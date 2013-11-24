dbe2adbd4697458eb07e5d7b34e0eeb08bbc2889
Paul <ageoldpun@gmail.com>
2013-11-23 23:56:10 -0800

    Finished the first step

--- lib/histogram.rb
+++ lib/histogram.rb
@@ -10,12 +10,20 @@ class Histogram
     prefixes.each do |prefix|
       prefix_hash[prefix] += 1
     end
-    
+
+    prefix_hash = prefix_hash.sort_by do |prefix, value|
+      value * -1
+    end
+
     prefix_string = ""
     prefix_hash.each do |prefix, count|
       prefix_string += "#{prefix} #{count}\n"
     end
-    prefix_string
+
+    prefix_array = prefix_string.split("\n")
+    prefix_array = prefix_array[0..6]
+
+    prefix_array.join("\n") + "\n"
   end
 end
 
--- spec/units/histogram_spec.rb
+++ spec/units/histogram_spec.rb
@@ -9,5 +9,13 @@ describe Histogram do
     it "Adds a running total" do
       subject.count_words("Mr. Jones\nMr. Piper").should == "Mr. 2\n"
     end
+
+    it "Sorts the results by frequency" do
+      subject.count_words("Ms. Piper\nMr. Jones\nMr. Poop").should == "Mr. 2\nMs. 1\n"
+    end
+
+    it "only returns the top 7 results" do
+      subject.count_words("Boba Piper\nBobb Jones\nBobc Poop\nBobd Piper\nBobe Jones\nBobf Poop\nBobg Piper\nBobh Jones\nBobi Poop").should == "Boba 1\nBobb 1\nBobc 1\nBobd 1\nBobe 1\nBobf 1\nBobg 1\n"
+    end
   end
 end
